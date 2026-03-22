97bdec15ad137622fc1ecbd9d728ab35ae69eea9
vee304 <vs055588@gmail.com>
2026-03-22 21:46:09 +0530

    Update er.go
    Stringer

--- Interfaces/er.go
+++ Interfaces/er.go
@@ -1,3 +1,30 @@
-// suffix - er
+// Stringer
+
+/*
+
+One of the most ubiquitous interfaces is Stringer defined by the fmt package.
+
+type Stringer interface {
+    String() string
+}
+A Stringer is a type that can describe itself as a string. The fmt package (and many others) look for this interface to print values.
+
+*/
 
 package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf(p.Name, p.Age)
+}
+
+func main() {
+	a := Person{"Pushkar", 20}
+	b := Person{"Love", 24}
+}
