2c960f9caf1ef438c9c872f7ac58e072c3b81aac
vee304 <vs055588@gmail.com>
2026-03-28 14:07:09 +0530

    Update multiinterface.go

--- Interfaces/multiinterface.go
+++ Interfaces/multiinterface.go
@@ -1,3 +1,26 @@
+/*
+
+Value receiver :- (d Dog)
+
+In this, Go makes a copy of the struct and gives that copy to the method.
+
+We use it only need to read data & not to change anything.
+
+
+
+Pointer Receiver : (d *Dog)
+
+Here, Go passes memory address the original struct
+
+if struct is massive, using a pointer is faster becasue Go doesn't have to spend time making copies
+
+
+A good rule of thumb for beginners:
+
+If any method of your struct needs to change data (a pointer receiver), make all the methods for that struct pointer receivers. It keeps things consistent.
+
+*/
+
 package main
 
 import "fmt"
